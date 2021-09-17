6fa561448150a94b43eb4908fa58d28cf3bff7d6
Andres Bravo <andresbravog@gmail.com>
2021-09-18 01:06:07 +0200

    Add control for API pagination limit

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -7,6 +7,7 @@ import requests
 import singer
 import singer.bookmarks as bookmarks
 import singer.metrics as metrics
+import re
 
 from singer import metadata
 
@@ -72,6 +73,9 @@ class ConflictError(GithubException):
     pass
 
 class RateLimitExceeded(GithubException):
+    pass
+
+class PaginationLimitExceeded(GithubException):
     pass
 
 class HttpException(Exception):
@@ -219,6 +223,10 @@ def authed_get(source, url, headers={}):
             if resp.status_code == 502 or resp.status_code == 503:
                 continue  # next attempt
 
+            # Handle 422 when reached github API pagination limit
+            if resp.status_code == 422 and re.match("pagination is limited for this resource.", resp.text):
+                raise PaginationLimitExceeded(resp.text)
+
             if resp.status_code != 200:
                 raise_for_error(resp, source)
             timer.tags[metrics.Tag.http_status_code] = resp.status_code
@@ -228,13 +236,22 @@ def authed_get(source, url, headers={}):
     raise HttpException(resp.text)
 
 def authed_get_all_pages(source, url, headers={}):
+    last_url = None
     while True:
-        r = authed_get(source, url, headers)
+        try:
+            r = authed_get(source, url, headers)
+        except PaginationLimitExceeded:
+            logger.info("API pagination limit exceeded for source %s.", source)
+            break
+
         yield r
+        if 'last' in r.links:
+            last_url = r.links['last']['url']
         if 'next' in r.links:
             url = r.links['next']['url']
         else:
             break
+
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
